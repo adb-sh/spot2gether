090a585a92b0d505401645e06dd3ecf0cb8e2629
adb-sh <git@adb.sh>
2022-10-02 00:20:50 +0200

    add dotenv

--- backend/store.mjs
+++ backend/store.mjs
@@ -1,6 +1,11 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 export const store = {
   users: [],
   clientID: process.env.APP_SPOTIFY_CLIENT_ID,
   clientSecret: process.env.APP_SPOTIFY_CLIENT_SECRET,
   redirectURL: process.env.APP_SPOTIFY_REDIRECT_URI,
 };
+
+console.log('store:', store);
